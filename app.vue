b537567935bd94e9548c26d7a2991771c779ef3b
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-08-13 13:01:15 -0300

    feat: add backend to frontend

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<script setup>
+  const showMessageFromBackend = async () => {
+    try {
+      const { data } = await useFetch('/server-api/test')
+      console.log(data.value)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+</script>
+
 <template>
   <div>
-    hello world
+     <button @click="showMessageFromBackend">Show message from backend</button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  serverMiddleware: [
+    { path: "/server-api", handler: "~/server-middleware/index.ts" },
+  ],
 })
